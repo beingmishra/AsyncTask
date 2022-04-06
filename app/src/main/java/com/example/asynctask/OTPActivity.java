07572679fe80abf03edb616cbd8ce7b19add1273
beingmishra <rahul731999@gmail.com>
2022-04-06 14:48:26 +0530

    OTP View

---
+++ app/src/main/java/com/example/asynctask/OTPActivity.java
@@ -0,0 +1,34 @@
+package com.example.asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class OTPActivity extends AppCompatActivity {
+
+    boolean isBackspaceClicked;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpactivity);
+
+
+        EditText otp_textbox_one, otp_textbox_two, otp_textbox_three, otp_textbox_four;
+
+        otp_textbox_one = findViewById(R.id.otp_edit_box1);
+        otp_textbox_two = findViewById(R.id.otp_edit_box2);
+        otp_textbox_three = findViewById(R.id.otp_edit_box3);
+        otp_textbox_four = findViewById(R.id.otp_edit_box4);
+
+
+        EditText[] edit = {otp_textbox_one, otp_textbox_two, otp_textbox_three, otp_textbox_four};
+
+        otp_textbox_one.addTextChangedListener(new GenericTextWatcher(otp_textbox_one, edit));
+        otp_textbox_two.addTextChangedListener(new GenericTextWatcher(otp_textbox_two, edit));
+        otp_textbox_three.addTextChangedListener(new GenericTextWatcher(otp_textbox_three, edit));
+        otp_textbox_four.addTextChangedListener(new GenericTextWatcher(otp_textbox_four, edit));
+    }
+}
